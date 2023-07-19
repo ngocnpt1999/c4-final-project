14e532e92a411a22e0ed7aae641071ef85a727a5
ngocnpt1999 <ngocnpt1999@gmail.com>
2023-07-19 15:41:29 +0700

    update todoAcess.ts

--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -10,3 +10,72 @@ const XAWS = AWSXRay.captureAWS(AWS)
 const logger = createLogger('TodosAccess')
 
 // TODO: Implement the dataLayer logic
+const documentClient = new DocumentClient();
+
+const tableName = process.env.TODO_TABLE_NAME;
+
+export async function findAllTodoByUserId(userId: string): Promise<TodoItem[]> {
+    const params: DocumentClient.QueryInput = {
+        TableName: tableName,
+        KeyConditionExpression: '#userId = :userId',
+        ExpressionAttributeNames: {
+            '#userId': 'userId'
+        },
+        ExpressionAttributeValues: {
+            ':userId': userId
+        }
+    };
+    const result = await documentClient.query(params).promise();
+    const items: TodoItem[] = result.Items as TodoItem[];
+    return items;
+}
+
+export async function create(item: TodoItem): Promise<TodoItem> {
+    const params: DocumentClient.PutItemInput = {
+        TableName: tableName,
+        Item: item
+    };
+    await documentClient.put(params).promise();
+    return item;
+}
+
+export async function update(
+    item: TodoUpdate,
+    todoId: string,
+    userId: string
+): Promise<TodoUpdate> {
+    const params: DocumentClient.UpdateItemInput = {
+        TableName: tableName,
+        Key: {
+            userId,
+            todoId
+        },
+        UpdateExpression: 'set #nameItem = :nameItem, #dueDateItem = :dueDateItem, #doneITem = :doneItem',
+        ExpressionAttributeNames: {
+            '#nameItem': 'name',
+            '#doneITem': 'done',
+            '#dueDateItem': 'dueDate'
+        },
+        ExpressionAttributeValues: {
+            ':nameItem': item.name,
+            ':doneITem': item.done,
+            ':dueDateItem': item.dueDate
+        },
+        ReturnValues: 'ALL_NEW'
+    };
+    const result = await documentClient.update(params).promise();
+    const updatedTodo: TodoUpdate = result.Attributes as TodoUpdate;
+    return updatedTodo;
+}
+
+export async function remove(todoId: string, userId: string): Promise<string> {
+    const params = {
+        Key: {
+            userId: userId,
+            todoId: todoId
+        },
+        TableName: tableName
+    };
+    await documentClient.delete(params).promise();
+    return '';
+}
