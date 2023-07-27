0a55f4e82c0e8bc531e321cdd71ffb15a8ae0e14
ngocnpt1999 <ngocnpt1999@gmail.com>
2023-07-27 11:33:17 +0700

    refactor code

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -1,8 +1,6 @@
-import * as AWS from 'aws-sdk'
 import { S3 } from 'aws-sdk';
-import * as AWSXRay from 'aws-xray-sdk'
 
-const XAWS = AWSXRay.captureAWS(AWS)
+// const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
 const s3 = new S3({ signatureVersion: 'v4' });
